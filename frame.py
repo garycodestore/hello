9a92f40f2f5780174bf7aa7c49b4284688a9e936
garycodestore <pro_standout@126.com>
2013-07-28 22:28:45 +0800

    update

--- frame.py
+++ frame.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 import wx
 
-class ExampleFrame(wx.Frame):
+class MyApp(wx.App):
+	def OnInit(self):
+		self.frame = MyFrame(None)
+		self.frame.Show()
+		return True
+
+
+class MyFrame(wx.Frame):
 	def __init__(self, parent):
 		wx.Frame.__init__(self, parent)
-		panel = wx.Panel(self)
-
-		self.quote = wx.StaticText(panel, label="your quote: ", pos=(20, 30))
 		self.logger = wx.TextCtrl(self, pos=(30, 20), size=(280, 300), style=wx.TE_MULTILINE|wx.TE_READONLY)
-	
-		self.button = wx.Button(self, label="Crawl", pos=(100, 325))	
-		self.button = wx.Button(self, label="Save", pos=(200, 325))
-
+		self.button1 = wx.Button(self, label="Crawl", pos=(100, 325))
+		self.button2 = wx.Button(self, label="Save", pos=(200, 325))
+		self.panel = wx.Panel(self)
 		self.Show()
-
-app = wx.App(False)
-ExampleFrame(None)
-app.MainLoop()
+if __name__ == "__main__":
+	app = MyApp(False)
+	app.MainLoop()
