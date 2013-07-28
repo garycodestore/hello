8d2c44a94359a350fb3dcc1058aba19ac40fa5ad
garycodestore <pro_standout@126.com>
2013-07-28 18:30:21 +0800

    update

--- frame.py
+++ frame.py
@@ -4,13 +4,13 @@ import wx
 class ExampleFrame(wx.Frame):
 	def __init__(self, parent):
 		wx.Frame.__init__(self, parent)
-		
 		panel = wx.Panel(self)
+
 		self.quote = wx.StaticText(panel, label="your quote: ", pos=(20, 30))
-		self.logger = wx.TextCtrl(self, pos=(300, 20), size=(200, 300), style=wx.TE_MULTILINE|wx.TE_READONLY)
-		
+		self.logger = wx.TextCtrl(self, pos=(30, 20), size=(280, 300), style=wx.TE_MULTILINE|wx.TE_READONLY)
+	
+		self.button = wx.Button(self, label="Crawl", pos=(100, 325))	
 		self.button = wx.Button(self, label="Save", pos=(200, 325))
-		#self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
 
 		self.Show()
 
